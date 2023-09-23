ba913acc3ac5ee206e44ab2d5b53754fcc2b9d72
Stephin Kipso <kipso.stephin@gmail.com>
2023-09-23 23:49:03 +0530

    added some change

--- src/main/java/com/example/demo/controllers/MonitoringController.java
+++ src/main/java/com/example/demo/controllers/MonitoringController.java
@@ -3,11 +3,28 @@ package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping(value="/monitor")
 public class MonitoringController {
     @GetMapping(value="/")
-    public int test() {
+    public int test() throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        InputStream inputStream = new FileInputStream("/Users/kipso/Downloads/demo/src/main/resources/alert_config.yaml");
+
+        System.out.println(inputStream);
+
+        HashMap yamlMap = yaml.load(inputStream);
+        for (Object o : yamlMap.entrySet()) {
+            System.out.println(o);
+        }
+
         return 777;
     }
 }
--- src/main/java/com/example/demo/services/MonitoringService.java
+++ src/main/java/com/example/demo/services/MonitoringService.java
@@ -1,7 +1,11 @@
 package com.example.demo.services;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableCaching
 public class MonitoringService {
+
+
 }
